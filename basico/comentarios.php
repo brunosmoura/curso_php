1035f894343c1590e632500d89694979979f6dac
brunosmoura <brunosmoura@outlook.com>
2021-03-03 11:40:26 -0300

    commit 03/03/2020

---
+++ basico/comentarios.php
@@ -0,0 +1,32 @@
+<div class="titulo">Comentários PHP</div>
+
+<?php
+
+echo "Estou no PHP";
+//echo "comentario";
+# echo "comentario";
+//  echo  #"comentario";
+#  echo  //"comentario";
+
+//
+?>
+
+<p>Depois do Primeiro Bloco!</p>
+<!--
+    <p>Depois do Primeiro Bloco!</p>
+
+    -->
+
+<?php
+
+/*
+Linha 1
+Linha 2
+Linha 3
+
+
+*/
+
+?>
+
+<p>Depois do segundo  Bloco!</p>
---
+++ basico/desafio.php
@@ -0,0 +1,7 @@
+<div class="titulo">Desafio</div>
+
+<Ul>
+    <li> 1 + 1 = <?php echo 1 + 1 ?></li>
+    <li> 2 + 2 = <?= 2 + 2; ?> </li>
+    <li>8 + 8 = <?= "1" ?> <?php echo 3+3; ?></li>
+</Ul>
