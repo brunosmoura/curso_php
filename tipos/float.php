63a2bd5b4fc9fbd84b5cc405df154c33e278053a
brunosmoura <brunosmoura@outlook.com>
2021-03-05 12:44:38 -0300

    terceiro commit

---
+++ tipos/float.php
@@ -0,0 +1,13 @@
+<div class="titulo">Tipo Float</div>
+
+<?php
+
+echo 1.1, '<br>';
+
+var_dump(1.1);
+echo '<br>';
+
+echo PHP_FLOAT_MAX, '<br>';
+echo PHP_FLOAT_MIN, '<br>';
+echo 1.2e3, '<br>'; // 1200 (1,2)^2
+echo 13e-3, '<br>'; //0.0013
---
+++ tipos/int.php
@@ -0,0 +1,18 @@
+<div class="titulo">Tipo inteiro</div>
+
+<?php
+
+echo 11;
+echo '<br>';
+
+var_dump(11); //mostra o tipo do dado
+
+echo '<br>';
+
+echo PHP_INT_MAX, '<br>'; //Maximo inteiro suportado pelo sistema
+echo PHP_INT_MIN, '<br>'; //Minimo inteiro suportado pelo sistema
+
+echo 017, '<br>'; // Base Octadecumal
+
+echo 0b11001111000, '<br>';  //Base binaria
+echo 0x117acf0; //Base exadecimal
